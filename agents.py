dd7f40b951cd5a3bcd027c5747c7fd2bba8afc08
Matthew Berman <31424909+TheMattBerman@users.noreply.github.com>
2024-04-29 21:34:05 +0000

    added temp grok, updates to agents

--- agents.py
+++ agents.py
@@ -3,6 +3,16 @@ from tools.search_tools import SearchTools
 
 
 class AINewsLetterAgents():
+    #def __init__(self):
+    #  self.llm = ChatGroq(
+    #      api_key=os.getenv("GROQ_API_KEY"),
+    #      model="mixtral-8x7b-32768"
+    #  )
+
+      # self.llm = ChatOpenAI(
+      #     model="gpt-4-turbo-preview",
+      # )
+  
     def editor_agent(self):
         return Agent(
             role='Editor',
@@ -17,7 +27,7 @@ class AINewsLetterAgents():
     def news_fetcher_agent(self):
         return Agent(
             role='NewsFetcher',
-            goal='Fetch the top AI news stories for the day',
+            goal='Fetch the top marketing, social, AI, tech news stories for the day',
             backstory="""As a digital sleuth, you scour the internet for the latest and most impactful developments
             in the world of marketing, social, AI, ensuring that our readers are always in the know.""",
             tools=[SearchTools.search_internet],
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os 
 import datetime
 from crewai import Crew, Process
+from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from agents import AINewsLetterAgents
 from tasks import AINewsLetterTasks
